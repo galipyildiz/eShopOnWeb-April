c9a3ddc77163f6a5d826995e646e2a73b600c10d
galipyildiz <galipyildiz9606@outlook.com>
2024-04-04 05:49:31 +0300

    Fix: orderDto add orderId

--- src/BlazorShared/Models/Order.cs
+++ src/BlazorShared/Models/Order.cs
@@ -3,6 +3,7 @@
 namespace BlazorShared.Models;
 public class Order
 {
+    public int Id { get; set; }
     public string BuyerId { get; set; }
     public DateTimeOffset OrderDate { get; set; }
     public OrderStateEnum OrderState { get; set; }
--- src/PublicApi/OrderEnpoints/OrderDto.cs
+++ src/PublicApi/OrderEnpoints/OrderDto.cs
@@ -5,8 +5,9 @@ namespace Microsoft.eShopWeb.PublicApi.OrderEnpoints;
 
 public class OrderDto
 {
-    public string BuyerId { get; private set; }
-    public DateTimeOffset OrderDate { get; private set; }
-    public OrderStateEnum OrderState { get; private set; } = OrderStateEnum.Approved;
-    public decimal TotalPrice { get; set; } = 0.0m;
+    public int Id { get; set; }
+    public string BuyerId { get; set; }
+    public DateTimeOffset OrderDate { get; set; }
+    public OrderStateEnum OrderState { get; set; }
+    public decimal TotalPrice { get; set; }
 }
